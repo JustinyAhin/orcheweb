610d0817a319bb40cfd67bb6b23cd416dd1b59e0
Justin Ahinon <justiny.ahinon@gmail.com>
2022-08-19 00:12:09 +0200

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,16 @@
-<section class="py-32 bg-jungle-green min-h-[70vh]">
+<section class="bg-jungle-green min-h-[70vh]">
 	<div class="mx-auto max-w-7xl">
-		<div class="space-y-6">
+		<header class="py-8">
+			<a href="/" class="text-light-cyan font-bold text-2xl underline">OrcheWeb</a>
+		</header>
+
+		<div class="space-y-6 pb-32 pt-20">
 			<h1 class="w-10/12 text-7xl font-bold text-light-cyan">
 				We create finely crafted websites for coaches and consultants.
 			</h1>
 			<p class="w-8/12 text-2xl text-light-cyan">
-				For a fixed monthly fee, we create a custom website for you, based on your needs, that will
-				help you build a successful online presence for your business.
+				For a fixed monthly fee, we create a <strong>custom website</strong> for you, based on your needs,
+				that will help you build a successful online presence for your business.
 			</p>
 
 			<a
