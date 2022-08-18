e1de68d1df4ed2c58691fef83653759008a108cb
Justin Ahinon <justiny.ahinon@gmail.com>
2022-08-19 00:05:36 +0200

    Home updates

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,27 @@
-<h1 class="text-5xl font-bold">OrcheWeb</h1>
+<section class="py-32 bg-jungle-green min-h-[70vh]">
+	<div class="mx-auto max-w-7xl">
+		<div class="space-y-6">
+			<h1 class="w-10/12 text-7xl font-bold text-light-cyan">
+				We create finely crafted websites for coaches and consultants.
+			</h1>
+			<p class="w-8/12 text-2xl text-light-cyan">
+				For a fixed monthly fee, we create a custom website for you, based on your needs, that will
+				help you build a successful online presence for your business.
+			</p>
+
+			<a
+				href="/"
+				class="flex py-4 px-6 text-xl font-bold bg-light-cyan text-jungle-green w-fit shadow-default"
+				>Our Pricing</a
+			>
+		</div>
+	</div>
+</section>
 
 <svelte:head>
 	<title>OrcheWeb</title>
+	<meta
+		name="description"
+		content="Subscription-based web development services for coachs and consultants."
+	/>
 </svelte:head>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,25 @@
 module.exports = {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 	theme: {
-		extend: {}
+		fontFamily: {
+			'work-sans': ['Work Sans', 'sans-serif']
+		},
+		colors: {
+			'jungle-green': '#092327',
+			'light-cyan': '#D7F9F1',
+			blue: {
+				600: '#2563EB'
+			},
+			gray: {
+				800: '#1F2937'
+			},
+			white: '#FFFFFF'
+		},
+		extend: {
+			boxShadow: {
+				default: '6px 6px 0 0 rgba(215, 249, 241, .25)'
+			}
+		}
 	},
 	plugins: []
 };
