5038ce9d55b23105ddbde9239c117485cfd1e27d
Justin Ahinon <justiny.ahinon@gmail.com>
2022-09-06 00:19:20 +0200

    Remove storyblok

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,6 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
-import storyblok from "@storyblok/astro";
-
-import * as dotenv from 'dotenv';
-dotenv.config();
 
 // https://astro.build/config
 export default defineConfig({
@@ -15,9 +11,6 @@ export default defineConfig({
         applyBaseStyles: false,
       },
     }),
-    sitemap(),
-    storyblok({
-      accessToken: process.env.STORYBLOCK_ACCESS_TOKEN
-    }),
+    sitemap()
   ],
 });
