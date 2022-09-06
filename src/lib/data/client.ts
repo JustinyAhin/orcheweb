3b3539a658d24f279a71421b38aea665f630b81e
Justin Ahinon <justiny.ahinon@gmail.com>
2022-09-06 06:48:56 +0200

    CS fixes

--- src/lib/data/client.ts
+++ src/lib/data/client.ts
@@ -1,10 +1,10 @@
-import { GraphQLClient } from 'graphql-request';
+import { GraphQLClient } from "graphql-request";
 import { getSdk } from "./storyblok";
 
-const client = new GraphQLClient('https://gapi.storyblok.com/v1/api', {
-    headers: {
-        Token: import.meta.env.STORYBLOCK_ACCESS_TOKEN
-    }
+const client = new GraphQLClient("https://gapi.storyblok.com/v1/api", {
+  headers: {
+    Token: import.meta.env.STORYBLOCK_ACCESS_TOKEN,
+  },
 });
 
 const sdk = getSdk(client);
--- src/lib/data/site-data.ts
+++ src/lib/data/site-data.ts
@@ -2,52 +2,52 @@ import { sdk } from "./client";
 import { renderRichText } from "@storyblok/astro";
 
 export type Feature = {
-    icon: {
-        filename: string;
-        alt: string;
-    };
-    title: string;
-    description: string;
-}
+  icon: {
+    filename: string;
+    alt: string;
+  };
+  title: string;
+  description: string;
+};
 
 // Site settings
 const {
-    data: {
-        SettingsItem: { content: settings },
-    },
+  data: {
+    SettingsItem: { content: settings },
+  },
 } = await sdk.SiteSettings();
 
 // Homepage
 const {
-    data: {
-        HomepageItem: { content: homepage }
-    }
+  data: {
+    HomepageItem: { content: homepage },
+  },
 } = await sdk.HomePage();
 
 homepage.features.forEach((feature: Feature) => {
-    feature.description = renderRichText(feature.description);
+  feature.description = renderRichText(feature.description);
 });
 
 // Case studies
 const {
-    data: {
-        CasestudyItems: { items: caseStudies }
-    }
+  data: {
+    CasestudyItems: { items: caseStudies },
+  },
 } = await sdk.CaseStudies();
 
-caseStudies.forEach(caseStudy => {
-    caseStudy.content.description = renderRichText(caseStudy.content.description);
+caseStudies.forEach((caseStudy) => {
+  caseStudy.content.description = renderRichText(caseStudy.content.description);
 });
 
 // FAQ
 const {
-    data: {
-        FaqItems: { items: faq }
-    }
+  data: {
+    FaqItems: { items: faq },
+  },
 } = await sdk.Faqs();
 
-faq.forEach(faqItem => {
-    faqItem.content.answer = renderRichText(faqItem.content.answer);
+faq.forEach((faqItem) => {
+  faqItem.content.answer = renderRichText(faqItem.content.answer);
 });
 
 export { settings, homepage, caseStudies, faq };
