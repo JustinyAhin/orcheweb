4ff4d4cfdb57578e16c3136e5cb292064ea51a3b
Justin Ahinon <justiny.ahinon@gmail.com>
2022-09-19 18:29:28 +0200

    Misc updates

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,13 +6,21 @@
 
 <MetaTags
 	title="OrcheWeb • Building open source apps for the web"
-	description="OrcheWeb is an 'lab' where we build open source apps for the web."
+	description="OrcheWeb is a 'lab' where we build open source apps for the web."
 	canonical={$page.url.href}
+	openGraph={{
+		images: [
+			{
+				url: 'https://res.cloudinary.com/orcheweb/image/upload/v1663602683/orcheweb/orcheweb-og-image_fqmitu.png',
+				width: 1500,
+				height: 780,
+				alt: 'OrcheWeb open graph image'
+			}
+		]
+	}}
 	twitter={{
 		cardType: 'summary_large_image',
-		handle: '@iamsegbedji',
-		image:
-			'https://res.cloudinary.com/orcheweb/image/upload/v1663602683/orcheweb/orcheweb-og-image_fqmitu.png'
+		handle: '@iamsegbedji'
 	}}
 />
 
@@ -21,10 +29,10 @@
 		<div class="pt-16 pb-32 space-y-6">
 			<h1 class="sm:w-10/12 text-light-cyan">Building open source apps for the web</h1>
 			<p class="text-2xl sm:w-8/12 text-light-cyan">
-				OrcheWeb is an "lab" where we build open source apps for the web.
+				OrcheWeb is a "lab" where we build open source apps for the web.
 			</p>
 
-			<a href="/tools" class="action-default bg-light-cyan text-jungle-green shadow-default"
+			<a href="/apps/gl" class="action-default bg-light-cyan text-jungle-green shadow-default"
 				>Our apps</a
 			>
 		</div>
