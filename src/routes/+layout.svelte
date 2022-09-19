70979615d6eac17650ecb79d51ff92c6be724930
Justin Ahinon <justiny.ahinon@gmail.com>
2022-09-19 17:54:16 +0200

    Update home page

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,5 +2,4 @@
 	import '../app.css';
 </script>
 
-<h1 class="text-3xl text-jungle-green">Hello there</h1>
 <slot />
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,32 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import { page } from '$app/stores';
+
+	import { MetaTags } from 'svelte-meta-tags';
+</script>
+
+<MetaTags
+	title="OrcheWeb - Building open source apps for the web"
+	description="OrcheWeb is an 'lab' where we build open source apps for the web."
+	canonical={$page.url.href}
+	twitter={{
+		cardType: 'summary_large_image',
+		handle: '@iamsegbedji',
+		image:
+			'https://res.cloudinary.com/orcheweb/image/upload/v1663602683/orcheweb/orcheweb-og-image_fqmitu.png'
+	}}
+/>
+
+<section class="bg-jungle-green h-screen flex items-center">
+	<div class="main-container">
+		<div class="pt-16 pb-32 space-y-6">
+			<h1 class="sm:w-10/12 text-light-cyan">Building open source apps for the web</h1>
+			<p class="text-2xl sm:w-8/12 text-light-cyan">
+				OrcheWeb is an "lab" where we build open source apps for the web.
+			</p>
+
+			<a href="/tools" class="action-default bg-light-cyan text-jungle-green shadow-default"
+				>Our apps</a
+			>
+		</div>
+	</div>
+</section>
