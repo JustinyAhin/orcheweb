f4668131dee18f81cf2c95d62a2c95175c5ce897
Justin Ahinon <justiny.ahinon@gmail.com>
2022-09-19 17:33:01 +0200

    CS fixes

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,31 +1,31 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{html,js,svelte,ts}'],
-  theme: {
-    fontFamily: {
-      "work-sans": ["Work Sans", "sans-serif"],
-    },
-    colors: {
-      "jungle-green": "#092327",
-      "light-cyan": "#D7F9F1",
-      "ruby-red": "#9B1D20",
-      blue: {
-        600: "#2563EB",
-      },
-      gray: {
-        400: "#9CA3AF",
-        600: "#4B5563",
-        700: "#374151",
-        800: "#1F2937",
-      },
-      white: "#FFFFFF",
-    },
-    extend: {
-      boxShadow: {
-        default: "6px 6px 0 0 rgba(215, 249, 241, .25)",
-        dark: "6px 6px 0 0 rgba(9, 35, 39, .25)",
-      },
-    },
-  },
-  plugins: [],
+	content: ['./src/**/*.{html,js,svelte,ts}'],
+	theme: {
+		fontFamily: {
+			'work-sans': ['Work Sans', 'sans-serif']
+		},
+		colors: {
+			'jungle-green': '#092327',
+			'light-cyan': '#D7F9F1',
+			'ruby-red': '#9B1D20',
+			blue: {
+				600: '#2563EB'
+			},
+			gray: {
+				400: '#9CA3AF',
+				600: '#4B5563',
+				700: '#374151',
+				800: '#1F2937'
+			},
+			white: '#FFFFFF'
+		},
+		extend: {
+			boxShadow: {
+				default: '6px 6px 0 0 rgba(215, 249, 241, .25)',
+				dark: '6px 6px 0 0 rgba(9, 35, 39, .25)'
+			}
+		}
+	},
+	plugins: []
 };
