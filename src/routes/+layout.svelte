412aff9a4216eab90efddd1c0701636e4eca23b3
Justin Ahinon <justiny.ahinon@gmail.com>
2023-01-31 11:56:19 +0200

    Add Pirsch Analytics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,4 +2,16 @@
 	import '../app.css';
 </script>
 
+<svelte:head>
+	{#if import.meta.env.PROD}
+		<!-- Pirsch Analytics -->
+		<script
+			defer
+			type="text/javascript"
+			src="https://api.pirsch.io/pirsch.js"
+			id="pirschjs"
+			data-code="E5MvxPstzAhl6udlESppDo72Bn8o3iSk"></script>
+	{/if}
+</svelte:head>
+
 <slot />
